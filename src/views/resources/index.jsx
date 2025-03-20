66fbb0396f5ab0a3a95bc391aad95891a8c3acd1
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2025-03-20 03:40:25 -0700

    Resource page changes

--- src/views/resources/index.jsx
+++ src/views/resources/index.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import './resources.css';
 
 const Resources = () => {
     return (
-        <p style={{"height": "67%", "display": "flex", "justifyContent": "center", "alignItems": "center"}}>Coming Soon.</p>
-    )
-}
+        <div>
+            {/* FAQ Section */}
+            <div className="faq-container">
+                <h1 className="faq-title">FAQs</h1>
+                <div className="faq-card">
+                    <span className="faq-question">Lorem ipsum lorem ipsum lorem ipsum?</span>
+                    <span className="faq-icon">V</span>
+                </div>
+                <div className="faq-card">
+                    <span className="faq-question">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum?</span>
+                    <span className="faq-icon">V</span>
+                </div>
+                <div className="faq-card">
+                    <span className="faq-question">Lorem ipsum lorem ipsum?</span>
+                    <span className="faq-icon">V</span>
+                </div>
+                <div className="faq-card">
+                    <span className="faq-question">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum?</span>
+                    <span className="faq-icon">V</span>
+                </div>
+            </div>
+
+            {/* Additional Resources Section */}
+            <div className="resources-container">
+                <h1 className="resources-title">Additional Resources</h1>
+                <div className="resources-grid">
+                    <div className="resource-item">
+                        <strong>Lorem ipsum</strong>
+                        <p>Lorem ipsum lorem ipsum</p>
+                    </div>
+                    <div className="resource-item">
+                        <strong>Lorem ipsum</strong>
+                        <p>Lorem ipsum lorem ipsum</p>
+                    </div>
+                    <div className="resource-item">
+                        <strong>Lorem ipsum</strong>
+                        <p>Lorem ipsum lorem ipsum</p>
+                    </div>
+                    <div className="resource-item">
+                        <strong>Lorem ipsum</strong>
+                        <p>Lorem ipsum lorem ipsum</p>
+                    </div>
+                    <div className="resource-item">
+                        <strong>Lorem ipsum</strong>
+                        <p>Lorem ipsum lorem ipsum</p>
+                    </div>
+                    <div className="resource-item">
+                        <strong>Lorem ipsum</strong>
+                        <p>Lorem ipsum lorem ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Resources;
