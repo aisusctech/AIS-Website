e9f5efcc4b6359d0d259317413492fed9ab29ff2
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2025-03-21 20:17:06 -0700

    Final

--- src/components/upcomingEvents/index.jsx
+++ src/components/upcomingEvents/index.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import GaneshChaturthi from "../../assets/diwali-1.svg";
-import Diwali from "../../assets/diwali-2.svg";
-import Holi from "../../assets/holi-2.svg";
 import "./upComingEvents.css";
 
 const UpComingEvents = () => {
@@ -10,21 +7,21 @@ const UpComingEvents = () => {
       <h1>Upcoming Events</h1>
       <div className='image-grid'>
         <div className='event-div'>
-          <div className='blur-bg' style={{ backgroundImage: `url(${GaneshChaturthi})` }}></div>
+          <div className='mask-bg event-1'></div>
           <div className='event-text'>
             <p>Ganesh Chaturthi</p>
             <p>27 August 2025</p>
           </div>
         </div>
         <div className='event-div'>
-          <div className='blur-bg' style={{ backgroundImage: `url(${Diwali})` }}></div>
+          <div className='mask-bg event-2'></div>
           <div className='event-text'>
             <p>Diwali</p>
             <p>20 October 2025</p>
           </div>
         </div>
         <div className='event-div'>
-          <div className='blur-bg' style={{ backgroundImage: `url(${Holi})` }}></div>
+          <div className='mask-bg event-3'></div>
           <div className='event-text'>
             <p>Garba</p>
             <p>30 September 2025</p>
