bfad4beef4cb695f7232c345cd254042c26b0d79
ammrit2312 <amriteshc101@icloud.com>
2025-09-23 21:32:40 -0700

    SEO first check

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import UpComingEvents from "../../components/upComingEvents";
 import Sponsor from "../../components/sponsor";
 import Testimonials from "../../components/testimonials";
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from "react-bootstrap/Spinner";
 import "./home.css";
 
 const Home = () => {
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -16,13 +16,24 @@ const Home = () => {
   }, []);
 
   return (
-    <div className={`home-container ${loading ? 'no-scroll' : ''}`}>
-      {
-      loading && <div className="loader"><Spinner animation="grow" role="status" /><span style={{marginLeft: "10px"}}>Loading...</span></div>
-      }
-      <div className='background-image-div'><p>Celebrating Culture, Connecting Communities!</p></div>
+    <div className={`home-container ${loading ? "no-scroll" : ""}`}>
+      <Helmet>
+        <title>AIS USC</title>
+        <meta name="description" content={`This page is the homepage for AIS USC. AIS is Association of Indian Students and supports Indian students and builds a community of Indian Students in the US.`} />
+        <meta property="og:title" content={`AIS USC`} />
+        <meta property="og:description" content={`This page is the homepage for AIS USC. AIS is Association of Indian Students and supports Indian students and builds a community of Indian Students in the US.`} />
+      </Helmet>
+      {loading && (
+        <div className="loader">
+          <Spinner animation="grow" role="status" />
+          <span style={{ marginLeft: "10px" }}>Loading...</span>
+        </div>
+      )}
+      <div className="background-image-div">
+        <p>Celebrating Culture, Connecting Communities!</p>
+      </div>
       <UpComingEvents />
-      <div className='background-image-div1'></div>
+      <div className="background-image-div1"></div>
       <Sponsor />
       <Testimonials />
     </div>
--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HelmetProvider } from "react-helmet-async";
 import Home from "./home";
 import Header from "../components/header";
 import Footer from "../components/footer";
@@ -8,25 +9,27 @@ import About from "./about";
 import Contact from "./contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UploadMemberDetails from "./uploadmemberdetails";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/Events" element={<Events />}></Route>
-          <Route path="/About" element={<About />}></Route>
-          <Route path="/Resources" element={<Resources />}></Route>
-          <Route path="/Contact" element={<Contact />}></Route>
-          <Route path="/Upload" element={<UploadMemberDetails />}></Route>
-        </Routes>
-        <Analytics />
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Events" element={<Events />}></Route>
+            <Route path="/About" element={<About />}></Route>
+            <Route path="/Resources" element={<Resources />}></Route>
+            <Route path="/Contact" element={<Contact />}></Route>
+            <Route path="/Upload" element={<UploadMemberDetails />}></Route>
+          </Routes>
+          <Analytics />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 };
 
